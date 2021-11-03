246a37c6af2c4c1e1caa55784bd3d21da38588a1
sarino22Y <sarino.malalanirina@esti.mg>
2021-11-03 08:55:01 +0300

    declare fichiers JS et CSS

--- web/app/themes/understrap/inc/enqueue.php
+++ web/app/themes/understrap/inc/enqueue.php
@@ -19,13 +19,15 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 
 		$css_version = $theme_version . '.' . filemtime( get_template_directory() . '/css/theme.min.css' );
 		wp_enqueue_style( 'understrap-styles', get_template_directory_uri() . '/css/theme.min.css', array(), $css_version );
-		wp_enqueue_style( 'understrap-child-styles', get_stylesheet_directory_uri() . '/css/sarino.css', wp_get_theme()->get('Version') );
+		wp_enqueue_style( 'understrap-child-styles1', get_stylesheet_directory_uri() . '/css/accueil.css', wp_get_theme()->get('Version') );
+		wp_enqueue_style( 'understrap-child-styles2', get_stylesheet_directory_uri() . '/css/accueilMeva.css', wp_get_theme()->get('Version') );
 
 		wp_enqueue_script( 'jquery' );
 
 		$js_version = $theme_version . '.' . filemtime( get_template_directory() . '/js/theme.min.js' );
 		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), $js_version, true );
-		wp_enqueue_script( 'understrap-child-scripts', get_stylesheet_directory_uri() .'/js/sarino.js', array('jquery'), $js_version, true );
+		wp_enqueue_script( 'understrap-child-scripts1', get_stylesheet_directory_uri() .'/js/accueil.js', array('jquery'), $js_version, true );
+		wp_enqueue_script( 'understrap-child-scripts2', get_stylesheet_directory_uri() .'/js/sarino.js', array('jquery'), $js_version, true );
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
