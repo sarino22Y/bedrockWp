f225a5194fa3f651cef8d9b2f3bcaa6a472fa03e
sarino22Y <sarino.malalanirina@esti.mg>
2021-11-10 12:05:22 +0300

    declaration jquery

--- web/app/themes/understrap/header.php
+++ web/app/themes/understrap/header.php
@@ -19,10 +19,16 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<script
-		src="js/jquery-3.6.0.min.js"
+		src="https://code.jquery.com/jquery-3.6.0.min.js"
 		integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
 		crossorigin="anonymous">
 	</script>
+	<script
+		src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"
+		integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30="
+		crossorigin="anonymous">
+	</script>
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 	<?php wp_head(); ?>
 </head>
 
